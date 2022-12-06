7d53ec7ac97c78a334d99b04492097971ab2ef8a
Kenzie <silvadesouzaanalice92@gmail.com>
2022-12-06 18:19:09 -0300

    concluido

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,3 +1,10 @@
+//Um analista quer conseguir ver quantos gols cada um de seus 5 atacantes marcou nas últimas 5 temporadas, e quer que você construa um código que mostre isso.
+
+//Crie um laco for que itere pelo array deixado no arquivo script.js, e em sequência itere pelo array que existe dentro do primeiro array. Você deve imprimir a temporada, e o número de gols, como no exemplo abaixo:
+
+//Jogador 1: 13, 15, 25, 34, 35
+//Jogador 2: 30, 48, 30, 12, 47
+
 const array = [
     [27, 4, 20, 13, 14],
     [11, 15, 12, 8, 9],
@@ -5,3 +12,36 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+/*
+for(let i = 0; i < array.length; i++){
+    let gols = 0;
+   
+    for(let j = 0; j < array[i].length;j++){
+     gols =  array[i];
+    }
+    console.log(`Jogador ${i+1}: [${gols}] `)
+}*/
+
+for(let i = 0; i < array.length; i++){
+    let gols = 0;
+    console.log(`Jogador ${i+1}: `)
+
+    for(let j = 0; j < array[i].length;j++){
+     gols = gols + array[i][j];
+     console.log(array[i][j])
+    }
+
+    console.log(`Total de gols = ${gols}`)
+
+    console.log("")
+}
+
+
+
+
+
+
+
+
+
